de12daa290033a7988b2192377d8750148e5b51e
Christoph Bühler <christoph@smartive.ch>
2015-08-07 10:44:05 +0200

    add more tests

--- test/appConfigTests.js
+++ test/appConfigTests.js
@@ -1,35 +1,35 @@
 var should = require('should');
 
 describe('Application config package', function () {
-    var Config = require('../index.js');
+    var appConfig = require('../index.js');
 
     describe('#constructor', function () {
         it('should return object', function () {
-            var config = new Config();
+            var config = appConfig();
             should.exist(config);
         });
 
         it('should pass default options', function () {
-            var config = new Config();
+            var config = appConfig();
             config.startupPath.should.be.equal(process.cwd());
         });
 
         it('should throw on wrong options', function () {
             should.throws(function () {
-                var config = new Config({
+                var config = appConfig({
                     startupPath: './'
                 });
             });
 
             should.throws(function () {
-                var config = new Config({
+                var config = appConfig({
                     configName: 'konfig.js'
                 });
             });
         });
 
         it('should return correct config', function () {
-            var config = new Config({
+            var config = appConfig({
                 localConfigName: 'noop.js'
             });
 
@@ -44,7 +44,7 @@ describe('Application config package', function () {
         });
 
         it('should merge config correctly with localconfig', function () {
-            var config = new Config();
+            var config = appConfig();
 
             should.exist(config.db);
             should.exist(config.very.nested.config);
@@ -54,6 +54,46 @@ describe('Application config package', function () {
 
             config.very.nested.config.variable.should.equal(true);
             config.very.nested.config.variableTwo.should.equal(true);
+        });
+
+        it('should disable isStage and isDebug', function () {
+            var config = appConfig({
+                enableStateVariables: false
+            });
+
+            should.not.exist(config.isDebug);
+            should.not.exist(config.isStage);
+        });
+
+        it('should configure isDebug correctly', function () {
+            var config = appConfig();
+
+            config.isDebug.should.equal(true);
+            config.isStage.should.equal(false);
+        });
+
+        it('should configure isStage correctly', function () {
+            var old = process.env.NODE_ENV;
+            process.env.NODE_ENV = 'staging';
+
+            var config = appConfig();
+
+            config.isDebug.should.equal(false);
+            config.isStage.should.equal(true);
+
+            process.env.NODE_ENV = old;
+        });
+
+        it('should configure production correctly', function () {
+            var old = process.env.NODE_ENV;
+            process.env.NODE_ENV = 'production';
+
+            var config = appConfig();
+
+            config.isDebug.should.equal(false);
+            config.isStage.should.equal(false);
+
+            process.env.NODE_ENV = old;
         });
 
         describe('environmental variables', function () {
@@ -66,7 +106,7 @@ describe('Application config package', function () {
             });
 
             it('should merge config correctly with environment variables', function () {
-                var config = new Config();
+                var config = appConfig();
 
                 should.exist(config.db);
                 should.exist(config.very.nested.config);
@@ -86,7 +126,7 @@ describe('Application config package', function () {
         });
 
         it('should reload and merge config correctly', function () {
-            var config = new Config();
+            var config = appConfig();
 
             config.db.user.should.equal('test');
 
